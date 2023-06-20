993d25ea088511ced49cf2bc76fb1255bc79dd68
ChanWaturandang <s22110059@student.unklab.ac.id>
2023-06-20 17:54:11 +0800

    header generation

---
+++ src/components/Generations.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Generations = () => {
+  return (
+    <div>
+      <h3>Generations Komponen</h3>
+      <ul>
+        <li>Generation 1</li>
+        <li>Generation 2</li>
+        <li>Generation 3</li>
+        <li>Generation 4</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Generations;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import Logo from './Logo';
+import Generations from './Generations';
+
+const Header = () => {
+  return (
+    <header>
+      <Logo />
+      <Generations />
+    </header>
+  );
+}
+
+export default Header;
